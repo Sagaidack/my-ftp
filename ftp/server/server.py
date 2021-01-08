b02af6f68c07c9054be302704f55fa6cfdf57d44
Sagaidak <gith.num1@gmail.com>
2021-01-08 12:11:07 +0200

    add a query method processing method and a header processing method

--- ftp/server/server.py
+++ ftp/server/server.py
@@ -21,8 +21,33 @@ class ServerSocket:
                client_sock, addr = self.process_client()
                self.satart_communication(client_sock)
     
-    def process_clietnt(self)
+    def _process_clietnt(self)
         return self.sock.accept()
 
     def start_communication(self, cl: socket.socket) -> None:
         handel_method(self, cl)
+
+    def _get_req_line(self, client_sock: socket.socket) -> str:
+        obj = ""
+        while True:
+            byte = self.client_sock.recv(1)
+            char = byte.decode("UTF-8")
+            if char == "\n":
+                break
+            obj = obj + char
+        return obj
+    
+    def get_method(self, cl: socket.socket) -> str:
+        return self._get_req_line(cl) 
+    
+    def get_header(self, cl: socket.socket) -> str:
+        return self._het_req_line(cl) 
+
+    def handle_method(self, cl: socket.socket) -> None:
+        self.get_method(cl)
+        if method == "Download":
+            self.send_file()
+        elif method == "Upload":
+            self.receive_file()
+        elif method == "Closed":
+            self.closed_connect()
